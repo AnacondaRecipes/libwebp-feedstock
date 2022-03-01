84a0480d9ae00759dc5b6a4e3da95ab2398798a1
leej3 <johnleenimh@gmail.com>
2022-03-01 11:59:10 -0500

    force shared libs

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,7 @@
 mkdir build
 cd build
 cmake -LAH -GNinja ..                                                  \
+    -DBUILD_SHARED_LIBS=ON                                             \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}                                   \
     -DCMAKE_BUILD_TYPE=Release
 
