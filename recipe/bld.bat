02479420b88c800a8be35e02bfa60a7291fff774
Kai Tietz <ktietz@anaconda.com>
2023-02-22 09:30:47 +0100

    enable gif tool

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,14 +10,14 @@ nmake /f Makefile.vc CFG=release-dynamic RTLIBCFG=dynamic OBJDIR=output UNICODE=
 if errorlevel 1 exit 1
 
 :: gif2webp is build separately and not included in the "all" target
-:: nmake /f Makefile.vc CFG=release-dynamic RTLIBCFG=dynamic OBJDIR=output UNICODE=0 gif2webp
+nmake /f Makefile.vc CFG=release-dynamic RTLIBCFG=dynamic OBJDIR=output UNICODE=0 gif2webp
 :: if errorlevel 1 exit 1
 
 copy output\release-dynamic\%ARCH%\bin\cwebp.exe %LIBRARY_PREFIX%\bin\cwebp.exe
 if errorlevel 1 exit 1
 copy output\release-dynamic\%ARCH%\bin\dwebp.exe %LIBRARY_PREFIX%\bin\dwebp.exe
 if errorlevel 1 exit 1
-:: copy output\release-dynamic\%ARCH%\bin\gif2webp.exe %LIBRARY_PREFIX%\bin\gif2webp.exe
+copy output\release-dynamic\%ARCH%\bin\gif2webp.exe %LIBRARY_PREFIX%\bin\gif2webp.exe
 :: if errorlevel 1 exit 1
 copy output\release-dynamic\%ARCH%\bin\img2webp.exe %LIBRARY_PREFIX%\bin\img2webp.exe
 if errorlevel 1 exit 1
